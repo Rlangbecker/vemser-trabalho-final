556dc492940eca69cb942e8faba70057dda2b0f6
Kaio Antonio <kaioaar@gmail.com>
2022-10-15 16:46:53 -0300

    add: Desafio CRUD

--- modulo-2/geekers/src/com/geekers/model/Desafio.java
+++ modulo-2/geekers/src/com/geekers/model/Desafio.java
@@ -5,5 +5,47 @@ public class Desafio {
     private Integer idDesafio;
     private String pergunta;
     private String resposta;
+    private Usuario usuario;
 
+    public Integer getIdDesafio() {
+        return idDesafio;
+    }
+
+    public void setIdDesafio(Integer idDesafio) {
+        this.idDesafio = idDesafio;
+    }
+
+    public String getPergunta() {
+        return pergunta;
+    }
+
+    public void setPergunta(String pergunta) {
+        this.pergunta = pergunta;
+    }
+
+    public String getResposta() {
+        return resposta;
+    }
+
+    public void setResposta(String resposta) {
+        this.resposta = resposta;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public String toString() {
+        return "Desafio{" +
+                "idDesafio=" + idDesafio +
+                ", pergunta='" + pergunta + '\'' +
+                ", resposta='" + resposta + '\'' +
+                ", usuario=" + usuario +
+                '}';
+    }
 }
--- modulo-2/geekers/src/com/geekers/view/Main.java
+++ modulo-2/geekers/src/com/geekers/view/Main.java
@@ -1,6 +1,9 @@
 package com.geekers.view;
 
+import com.geekers.exceptions.BancoDeDadosException;
+import com.geekers.model.Desafio;
 import com.geekers.model.Usuario;
+import com.geekers.service.DesafioService;
 import com.geekers.service.UsuarioService;
 
 import java.time.LocalDate;
@@ -8,20 +11,36 @@ import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws BancoDeDadosException {
         Scanner scanner = new Scanner(System.in);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         Usuario usuario = new Usuario();
+        Desafio desafio = new Desafio();
+        Desafio desafioEditado = new Desafio();
         UsuarioService usuarioService = new UsuarioService();
+        DesafioService desafioService = new DesafioService();
 
-        usuario.setNome("Legado");
+        usuario.setNome("Teste");
         usuario.setEmail("Kaio@bol.com");
         usuario.setDataNascimento(LocalDate.parse("14/03/2002", formatter));
         usuario.setSenha("1234");
         usuario.setTelefone("9991283113");
         usuario.setSexo("M");
-        usuarioService.adicionarUsuario(usuario);
-        usuarioService.listarUsuarios();
+
+        desafio.setPergunta("gremio maior do rio?");
+        desafio.setResposta("1");
+        desafio.setUsuario(usuario);
+
+        desafioEditado.setPergunta("O flamengo é o maior de todos os tempos?");
+        desafioEditado.setResposta("1");
+        //usuarioService.adicionarUsuario(usuario);
+        //desafioService.adicionarDesafio(desafio);
+        //desafioService.listarDesafioPorUsuario(9);
+        //desafioService.editarDesafio(4, desafioEditado);
+        //desafioService.listarDesafioPorUsuario(9);
+        //desafioService.removerDesafio(10);
+        //desafioService.verificarResposta("1",9);
+        //usuarioService.listarUsuarios();
 
     }
 }
